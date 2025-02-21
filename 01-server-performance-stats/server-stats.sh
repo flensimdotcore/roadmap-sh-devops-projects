bb4a4f9a13a15b4660a57614215cdc13157f9e0a
Ivan Maraev <ivanmaraev@internet.ru>
2025-02-21 14:42:44 +0300

    add: load average

--- 01-server-performance-stats/server-stats.sh
+++ 01-server-performance-stats/server-stats.sh
@@ -10,8 +10,12 @@ df -m / | awk 'NR==2 {print "used disk: " $3 " Mb (" 100*$3/$2 "%)"}'
 df -m / | awk 'NR==2 {print "free disk: " $4 " Mb (" 100*$4/$2 "%)"}'
 
 # Top 5 processes by CPU usage
+# echo -n 'top PIDs by disk usage: '
+# df -m /
 
 # Top 5 processes by memory usage
+echo -n 'top PIDs by memory usage: '
+ps -eo pid,%mem --sort=-%mem | head -6 | awk 'NR>1 {printf "%s (%.2f%%), ", $1, $2}' | sed 's/, $/\n/'
 
 # OS version
 cat /proc/version | awk '{printf "os version: %s %s %s %s\n", $1, $3, $6, $7}'
@@ -20,6 +24,7 @@ cat /proc/version | awk '{printf "os version: %s %s %s %s\n", $1, $3, $6, $7}'
 cat /proc/uptime | awk '{printf "system uptime: %s s\n", $1}'
 
 # Load average
+cat /proc/loadavg | awk '{printf "load average (1 min, 5 min, 15 min): %s, %s, %s\n", $1, $2, $3}'
 
 # Logged in users
 
